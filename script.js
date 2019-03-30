6083d98ef7515d5f86d4ea371e374cfd2d389f24
Heartsonsunday <leahgdg@hotmail.com>
2019-03-30 11:06:49 -0400

    Required functions

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-var todo = ["item_1", "item_2", "item_3", "item_4"]; //place to store todos
+var todo = ["item_1", "item_2", "item_3", "item_4"]; //todos Array
 
-Console.log("My ToDos", todo);
+
+function displaytd() {
+
+Console.log("My ToDos", todo); //displaying
+}
+
+function addtd(x) {
+todo.push(x);  //adding to array
+displaytd();
+}
+
+function changetd(x, newtd) {
+todo[x] = newtd; //identify and change todos by index
+displaytd();
+}
+
+function deletetd () {
+todo.splice(x,y); //remove todo w/ splice (index, Qt);
+displaytd();
+}
